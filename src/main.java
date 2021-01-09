3a895a93733ffcdab61798d47866bd8d210a3029
Alexsandr Orlov <zipoz1@rambler.ru>
2021-01-09 22:38:26 +0300

    inicial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main  {
+    public static void main(String[] args) {
+        int price = 1250;
+        int bonus = 20;
+        int miles = price / bonus;
+
+        System.out.println(miles);
+    }
+}
